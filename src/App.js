279bc7b565f89dd3fb5ac3ebf5e3bdbb33c01851
suyeun84 <81475092+suyeun84@users.noreply.github.com>
2022-12-30 04:47:39 +0900

    basis setting

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 해보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        //map : 불변성 유지하면서 특정 배열 원소 업데이트
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
+
+/* 4개의 컴포넌트
+1.TodoTemplate
+-> 화면 가운데 정렬, 앱 타이틀(일정 관리)을 보여줌, children으로 내부 JSX를 props로 받아 와서 렌더링
+2.TodoInsert
+-> 새로운 항목 입력하고 추가, state 통해 인풋의 상태 관리
+3.TodoListItem
+-> 각 할 일 항목에 대한 정보 보여줌, todo 객체를 props로 받아 와서 상태에 따라 다른 스타일의 UI 보여줌
+4.TodoList
+-> todos 배열을 props로 받아 온 후, 이를 배열 내장 함수 map을 사용해서 여러 개의 TodoListItem 컴포넌트로 변환하여 보여줌
+*/
